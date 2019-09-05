9dde2407aea83a2dd860623dc649c7539d9db288
rsoriano <rsorianosanchis@gmail.com>
2019-09-06 01:17:56 +0200

    version 06/09/2019--01:17

--- src/components/AuthorItem.js
+++ src/components/AuthorItem.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 
 function AuthorItem({ author }) {
     return (
-        <div className="card mb-3" >
+        <div className="card mb-5" >
             <h3 className="card-header">{author.name}</h3> 
-            <img alt='' src={`../${author.avatar.path}`}/> 
+            <div>
+                <img alt='Card image' src={`http://localhost:8080/${author.avatar.path}`} width="100%"/> 
+            </div>
             <div className="card-body">
                 <p className="card-text">{author.description}</p>
             </div>
